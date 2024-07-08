03055a2c8badaaf98132388d3d984062f367e7d2
michaelwestblade <michaelwestblade@gmail.com>
2024-07-08 13:15:49 -0500

    maxChar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+
+    for(const char of str) {
+        if(!charMap[char]) {
+            charMap[char] = 1;
+        }else {
+            charMap[char] += 1;
+        }
+    }
+
+    return Object.entries(charMap).reduce((acc, [character, count]) => {
+        if(count > acc.count) {
+            acc.character = character;
+            acc.count = count;
+        }
+        return acc;
+    }, {'character': '', 'count': 0}).character
+}
+
+console.log('maxChar("abcccccccd") === "c"', maxChar("abcccccccd") === "c");
+console.log('maxChar("apple 1231111") === "1"', maxChar("apple 1231111") === "1");
 
 module.exports = maxChar;
