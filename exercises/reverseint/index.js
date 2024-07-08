ec153efe0885a5b4d055d1bacfdfdf8a400fa321
michaelwestblade <michaelwestblade@gmail.com>
2024-07-08 13:09:43 -0500

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const sign = Math.sign(n); // n/Math.abs(n)
+    // return sign * parseInt(n.toString().split('').reverse().join(''))
+    return sign * parseInt(`${n}`.split('').reverse().join(''))
+}
+
+console.log('reverseInt(15) === 51', reverseInt(15) === 51);
+console.log('reverseInt(981) === 189', reverseInt(981) === 189);
+console.log('reverseInt(500) === 5', reverseInt(500) === 5);
+console.log('reverseInt(-15) === -51', reverseInt(-15) === -51);
+console.log('reverseInt(-90) === -9', reverseInt(-90) === -9);
 
 module.exports = reverseInt;
