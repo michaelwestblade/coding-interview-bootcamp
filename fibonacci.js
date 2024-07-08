07a827099c84a78e6b70c89838750bcbfb57a9b1
michaelwestblade <michaelwestblade@gmail.com>
2024-07-08 10:29:17 -0500

    adding course content

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+const memo = [];
+const fibonacci = (n) => {
+    if(n <= 1) {
+        return n
+    }
+
+    if(memo[n]) {
+        return memo[n]
+    }
+
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    return memo[n]
+}
+
+console.log(fibonacci(5))
