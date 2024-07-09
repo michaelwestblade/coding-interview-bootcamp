e32a2d5bf9133d0b536432b40e0fd15bd5ccc24c
michaelwestblade <michaelwestblade@gmail.com>
2024-07-08 21:16:43 -0500

    creating n steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,61 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    const charArray = [];
+    const spacesArray = [];
+
+    for (let i = 1; i < n; i++) {
+        spacesArray.push(" ");
+    }
+
+    for (let i = 1; i <= n; i++) {
+        charArray.push("#");
+        console.log(`${charArray.join("")}${spacesArray.join("")}`);
+        spacesArray.pop()
+    }
+}
+
+function steps2(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+        for (let col = 0; col < n; col++) {
+            if (col <= row) {
+                stair += "#";
+            } else {
+                stair += " ";
+            }
+        }
+        console.log(stair);
+    }
+}
+
+function stepsRecursive(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return stepsRecursive(n, row + 1, '');
+    }
+
+    if (stair.length <= row) {
+        stair += "#";
+    } else {
+        stair += " ";
+    }
+    return stepsRecursive(n, row, stair);
+}
+
+steps(4)
+
+console.log(" -------- ");
+
+steps2(4);
+
+console.log(" -------- ");
+
+stepsRecursive(5)
 
 module.exports = steps;
