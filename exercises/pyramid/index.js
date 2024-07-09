973ecee38c874a55be1ea6ee7201468c55a7c4a6
michaelwestblade <michaelwestblade@gmail.com>
2024-07-09 11:07:45 -0500

    console log a pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,75 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const width = (2*n) - 1;
+    for (let i = 1; i <= width; i+=2) {
+        const spaces = width - i;
+        let precedingSpaces = spaces > 0 ? Math.floor(spaces / 2) : 0;
+        let trailingSpaces = spaces > 0 ? Math.floor(spaces / 2) : 0;
+        let characters = i;
+        let stringToPrint = "";
+
+        while(precedingSpaces > 0) {
+            stringToPrint += " ";
+            precedingSpaces -= 1;
+        }
+
+        while(characters > 0) {
+            stringToPrint += "#";
+            characters -= 1;
+        }
+
+        while(trailingSpaces > 0) {
+            stringToPrint += " ";
+            trailingSpaces -= 1;
+        }
+
+        console.log(stringToPrint);
+    }
+}
+
+function pyramid2(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    for (let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if (midpoint - row <= column && midpoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+
+        console.log(level);
+    }
+}
+
+function pyramidRecursive(n, row=0, level = '') {
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramidRecursive(n, row + 1);
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add = '';
+
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#'
+    } else {
+        add = ' ';
+    }
+
+    pyramidRecursive(n, row, level+add);
+}
+
+pyramid(4)
+pyramid2(4)
+pyramidRecursive(4)
 
 module.exports = pyramid;
