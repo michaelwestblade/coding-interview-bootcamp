f54da0b87f9e36436f6fdae7d8d2f511a2da1791
michaelwestblade <michaelwestblade@gmail.com>
2024-07-09 11:38:36 -0500

    finding vowel count

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,7 +6,31 @@
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
+const VOWELS = ['a', 'e', 'i', 'o', 'u'];
 
-function vowels(str) {}
+function vowels(str) {
+    let vowelCount = 0;
+
+    for (const char of str.toLowerCase()) {
+        if (VOWELS.includes(char)) {
+            vowelCount++;
+        }
+    }
+
+    return vowelCount;
+}
+
+function vowels2(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+console.log(`vowels('Hi There!') --> 3`, vowels('Hi There!'));
+console.log(`vowels('Why do you ask?') --> 4`, vowels('Why do you ask?'));
+console.log(`vowels('Why?') --> 0`, vowels('Why?'));
+
+console.log(`vowels('Hi There!') --> 3`, vowels2('Hi There!'));
+console.log(`vowels('Why do you ask?') --> 4`, vowels2('Why do you ask?'));
+console.log(`vowels('Why?') --> 0`, vowels2('Why?'));
 
 module.exports = vowels;
