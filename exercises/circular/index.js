c5da4d48e93c21cacb80d7d9968723804ef3e13e
michaelwestblade <michaelwestblade@gmail.com>
2024-07-11 19:42:17 -0500

    circular linked list detection

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,23 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    if (!list.head.next) {
+        return false;
+    }
+
+    let slow = list.head;
+    let fast = list.head;
+
+    while(fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 module.exports = circular;
